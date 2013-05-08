b4201d291231d6712d47c07031e3edd9029d78d4
Linus Wallin <linuswallin8@hotmail.com>
2013-05-08 14:49:08 +0200

    Compressed all the js and css files, One file to rule them all

---
+++ assets/twitter-bootstrap-37d0a30/tinlin/js/custom.js
@@ -0,0 +1,25 @@
+
+jQuery(document).ready(function($) {
+	$(".collapse-alt").collapse();
+
+	$('#demo').mobiscroll().date({
+		theme : 'jqm',
+		display : 'bubble',
+		mode : 'scroller',
+		dateOrder : 'ddMyyyy',
+		dateFormat : 'yyyy-mm-dd',
+	});
+
+	$('#show_history_info').click(function() {
+		if ($(this).hasClass('hide_history_info')) {
+			$('.history .hiddeninfo').removeClass('hide');
+			$(this).removeClass('hide_history_info');
+		} else {
+			$('.history .hiddeninfo').addClass('hide');
+			$(this).addClass('hide_history_info');
+		}
+	});
+
+	$('#demo').scroller('setDate', now, true);
+
+});
